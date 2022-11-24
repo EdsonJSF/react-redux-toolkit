5d38455f7e40424d899317ac96ede2e6d40fb2a8
Edsonjsf <edsonjsf1990@gmail.com>
2022-11-24 14:56:31 -0400

    Creacion del pokemonSlice

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
+import { PokemonApp } from "./pokemonApp";
 import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <App /> */}
+      <PokemonApp></PokemonApp>
     </Provider>
   </React.StrictMode>
 );
